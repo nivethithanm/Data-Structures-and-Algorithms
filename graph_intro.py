31b96732c33f78775f6c9ce9bfa029d91c30bbf8
Nivethithan <46256177+nivethithanm@users.noreply.github.com>
2021-01-02 16:04:46 +0530

    Update graph_intro.py

--- graph_intro.py
+++ graph_intro.py
@@ -1,3 +1,16 @@
+### Problem
+#You have been given an undirected graph consisting of N nodes and M edges. This graph can consist of self-loops as well as multiple edges. In addition , you have also been given Q queries. For each query , you shall be given 2 integers A and B. You just need to find if there exists an edge between node A and node B. If yes, print "YES" (without quotes) else , print "NO"(without quotes).
+
+#Input Format:
+
+#The first line consist of 2 integers N and M denoting the number of nodes and edges respectively. Each of the next M lines consist of 2 integers A and B denoting an undirected edge between node A and B. The next line contains a single integer Q denoting the number of queries. The next Line contains 2 integers A and B denoting the details of the query.
+
+#Output Format
+
+#Print Q lines, the answer to each query on a new line.
+
+### Code
+
 arr_ = input().split()
 
 N = int(arr_[0])
